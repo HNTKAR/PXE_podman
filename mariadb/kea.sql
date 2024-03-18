8c31f9ee3d92bfd2b16275d22f66b80b2fc15646
HNTKAR <45356209+HNTKAR@users.noreply.github.com>
2024-03-18 15:51:31 +0900

    remove docker-run

--- mariadb/kea.sql
+++ mariadb/kea.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE kea_db;
 CREATE USER 'kea_user'@'localhost' IDENTIFIED BY 'kea_password';
 GRANT ALL ON kea_db.* TO 'kea_user'@'localhost';
-SET GLOBAL innodb_flush_log_at_trx_commit=2;
+SET @@global.log_bin_trust_function_creators = 1;
